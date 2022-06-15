f17f9949b2a565e2a1d0b9d7ddfd5824e7c7e3c1
codingdrizzle <codingfrancis100@gmail.com>
2022-06-15 01:39:37 +0000

    Changes to the server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const routes = require('./routes')
@@ -7,12 +7,13 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use('/api/', bodyParser.json(), routes)
+app.use(cors())
 app.use(express.json())
+app.use('/api', routes)
 
 
 
-mongoose.connect('mongodb+srv://codingdrizzle:gateway_api${user}@todolist.vczak00.mongodb.net/todoList' || process.env.DATABASE_URL)
+mongoose.connect(process.env.MONGODB_URL || process.env.DATABASE_URL)
         .then(() => console.log('connected to db'))
         .catch((e) => console.error(e))
 
